9b6471ca1cd3fad4dcfb9f7eb071ef81071203be
yxxs1993 <1113799552@qq.com>
2017-11-16 08:11:32 +0800

    发布话题界面

---
+++ app/src/main/java/com/busilinq/casepocket/ui/PostActiveActivity.java
@@ -0,0 +1,29 @@
+package com.busilinq.casepocket.ui;
+
+import com.busilinq.casepocket.base.BaseActivity;
+
+/**
+ * 描述 ：发布话题
+ * <p>
+ * 邮箱：shiquan.lu@busilinq.com
+ * 创建时间：2017/11/15
+ * author: shiquan.lu
+ */
+
+public class PostActiveActivity extends BaseActivity {
+
+    @Override
+    public int initContentView() {
+        return 0;
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initUi() {
+
+    }
+}
